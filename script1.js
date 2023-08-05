088c7def9c3688e20de4301ceafeabda27b4e5ff
Gyanthakur <98226958+Gyanthakur@users.noreply.github.com>
2023-08-05 08:34:37 +0530

    5th commit

--- script1.js
+++ script1.js
@@ -18,18 +18,18 @@ document.getElementById("loginForm").addEventListener("submit", function (event)
     }
 });
 
-document.getElementById("signupForm").addEventListener("submit", function (event) {
-    event.preventDefault();
-    const username = document.getElementById("username").value;
-    const email = document.getElementById("email").value;
-    const phone = document.getElementById("phone").value;
-    const password = document.getElementById("password").value;
-    // Replace this with actual signup logic.
-    // For the sake of simplicity, we are allowing any non-empty username/email/password combination.
-    if (username.trim() && email.trim() && phone.trim() && password.trim()) {
-        sessionStorage.setItem("isLoggedIn", "true");
-        window.location.href = "index.html";
-    } else {
-        alert("Invalid signup information. Please fill all fields and try again.");
-    }
-});
+// document.getElementById("signupForm").addEventListener("submit", function (event) {
+//     event.preventDefault();
+//     const username = document.getElementById("username").value;
+//     const email = document.getElementById("email").value;
+//     const phone = document.getElementById("phone").value;
+//     const password = document.getElementById("password").value;
+//     // Replace this with actual signup logic.
+//     // For the sake of simplicity, we are allowing any non-empty username/email/password combination.
+//     if (username.trim() && email.trim() && phone.trim() && password.trim()) {
+//         sessionStorage.setItem("isLoggedIn", "true");
+//         window.location.href = "index.html";
+//     } else {
+//         alert("Invalid signup information. Please fill all fields and try again.");
+//     }
+// });
